9f4adc59fa0768870b66d2bcd5aef71d9732fe86
Fayaz <d00217205@student.dkit.ie>
2022-05-05 00:48:55 +0100

    Create Blog page: added border, shadow, organized and editted each input section, added heading to each input section, added back to main page button and changed sizes

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+
+{{-- <p class="text-base md:text-sm text-green-500 font-bold">&lt; 
+<a href="#" class="text-base md:text-sm text-green-500 font-bold no-underline hover:underline">BACK TO HOME</a></p> --}}
+
 <div class="w-4/5 m-auto text-left">
+
+    <p class="text-base md:text-sm text-red-900 font-bold pt-5 pl-5">&lt; 
+    <a href="#" class="text-base md:text-sm text-red-900 font-bold no-underline hover:underline">BACK TO HOME</a></p>
+        
+
     <div class="py-15">
-        <h1 class="text-6xl">
-            Create Post
+        <h1 class="text-5xl text-red-900">
+            Create Blog Post
         </h1>
     </div>
 </div>
@@ -21,87 +30,117 @@
     </div>
 @endif
 
-<div class="w-4/5 m-auto pt-20">
+<div class="w-4/5 m-auto p-10 border shadow-lg">
     <form 
         action="/blog"
         method="POST"
         enctype="multipart/form-data">
         @csrf
 
+        <h3 class="text-2xl font-semibold">Country Name:</h3>
         <input 
             type="text"
             name="countryName"
-            placeholder="Country Name..."
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+            placeholder="Please type in Country Name here..."
+            class="bg-transparent block border-b-1 w-full h-10 text-2xl outline-none">
+            
+            <br>
 
+        <h3 class="text-2xl font-semibold">Continent ID:</h3>
         <input 
             type="text"
             name="continentID"
-            placeholder="continentID..."
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+            placeholder="Please type in Continent ID here..."
+            class="bg-transparent block border-b-1 w-full h-10 text-2xl outline-none">
 
+            <br>
+        <h3 class="text-2xl font-semibold">Language ID:</h3>
         <input 
             type="text"
             name="languageID"
-            placeholder="languageID..."
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
-        
+            placeholder="Please type in Language ID here..."
+            class="bg-transparent block border-b-1 w-full h-10 text-2xl outline-none">
+            
+            <br>
+            <h3 class="text-2xl font-semibold">Capital Name:</h3>
         <input 
             type="text"
             name="capital"
-            placeholder="capital..."
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+            placeholder="Please type in capital of country here..."
+            class="bg-transparent block border-b-1 w-full h-10 text-2xl outline-none">
         
+            <br>
+            <h3 class="text-2xl font-semibold">Population:</h3>
         <input 
         type="text"
         name="population"
-        placeholder="population..."
-        class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+        placeholder="Please type in population of country here..."
+        class="bg-transparent block border-b-1 w-full h-10 text-2xl outline-none">
 
+        <br>
+
+        <h3 class="text-2xl font-semibold">Country Area:</h3>
         <input 
         type="text"
         name="area"
-        placeholder="area..."
-        class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+        placeholder="Please type in area here..."
+        class="bg-transparent block border-b-1 w-full h-10 text-2xl outline-none">
 
+        <br>
+
+        <h3 class="text-2xl font-semibold">Country's Population Density:</h3>
         <input 
         type="text"
         name="popDensity"
-        placeholder="popDensity..."
-        class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+        placeholder="Please type in Population Density of country here..."
+        class="bg-transparent block border-b-1 w-full h-10 text-2xl outline-none">
 
+        <br>
+
+        <h3 class="text-2xl font-semibold">History:</h3>
         <textarea 
             name="history"
-            placeholder="history..."
-            class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
+            placeholder="Please type in History of country here..."
+            class="py-20 bg-transparent block border-b-1 w-full h-60 text-2xl outline-none"></textarea>
 
+            <br>
+
+            <h3 class="text-2xl font-semibold">Culture:</h3>     
         <textarea 
         name="culture"
-        placeholder="culture..."
-        class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
+        placeholder="Please type in Culture here..."
+        class="py-20 bg-transparent block border-b-1 w-full h-60 text-2xl outline-none"></textarea>
 
+        <br>
+
+        <h3 class="text-2xl font-semibold">Food:</h3>
         <textarea 
         name="food"
-        placeholder="food..."
-        class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
+        placeholder="Please type in about Food here..."
+        class="py-20 bg-transparent block border-b-1 w-full h-60 text-2xl outline-none"></textarea>
 
+        <br>
+
+        <h3 class="text-2xl font-semibold">Tourism:</h3>
         <textarea 
         name="tourism"
-        placeholder="tourism..."
-        class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
+        placeholder="Please type in about Tourism here..."
+        class="py-20 bg-transparent block border-b-1 w-full h-60 text-2xl outline-none"></textarea>
 
-
-        {{-- <div class="bg-grey-lighter pt-15">
+        <br>
+        
+        <h3 class="text-2xl pl-2 font-semibold"> Upload picture</h3>
+        <div class="bg-grey-lighter pt-1">
             <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
                 <span class="mt-2 text-base leading-normal">
-                    Select a file
+                    Select a file(s)
                 </span>
                 <input 
                     type="file"
                     name="image"
                     class="hidden">
             </label>
-        </div> --}}
+        </div>
 
         <button    
             type="submit"
